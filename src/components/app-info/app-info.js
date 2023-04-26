0a207e093fa4dcf3f6481357325f7e58b248f498
shaldrg <100235810+shaldrg@users.noreply.github.com>
2023-04-26 16:29:03 +0300

    Update app-info.js

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import "../app-info/app-info.css";
 const AppInfo = () => {
   return (
     <div className="app-info">
-      <h1>Учет сотрудников в компании NigaOne</h1>
+      <h1>Учет сотрудников в компании</h1>
       <h2>Общее число сотрудников: </h2>
       <h2>Премию получат:</h2>
     </div>
